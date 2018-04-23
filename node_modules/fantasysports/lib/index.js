a40c302a69c5733f273dc909e76240b49d9ae02f
Camden <crt2004@wildcats.unh.edu>
2018-04-23 18:57:50 -0400

    Remove unnecessary directory

--- node_modules/fantasysports/lib/index.js
+++
@@ -1,6 +0,0 @@
-var _ = require("underscore");
-var FantasySports = require("./framework/fantasy");
-var Auth = require("./framework/auth");
-var FantasyRequest = ("./framework/fantasyRequest");
-
-module.exports = new FantasySports(new Auth());
