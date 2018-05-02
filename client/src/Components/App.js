cc18722dbb720539fcb8d954c15d14e6826e8cc4
camdent719 <34377169+camdent719@users.noreply.github.com>
2018-05-02 10:11:52 -0400

    Add files via upload

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -15,6 +15,26 @@ export default class App extends Component {
     };
   }
 
+  state = {
+    response: ''
+  };
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res.express }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/auth/test');
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  };
+
+
   // function frontPage(props) {
   //   const isLoggedIn = props.isLoggedIn;
   //   if (isLoggedIn) {
