0eb722129af06e6049626881ba13c5549d6a41b7
camdent719 <34377169+camdent719@users.noreply.github.com>
2018-05-01 22:46:08 -0400

    Add files via upload

--- client/src/Components/Display/Login.js
+++ client/src/Components/Display/Login.js
@@ -1,1 +1,31 @@
+import React, {Component, Fragment} from 'react'
+import { Typography, Button } from 'material-ui'
 
+export default class Login extends Component {
+  state = { data };
+
+  getYahooData() {
+    fetch('/auth/test')
+      //.then(res => res.json())
+      //.then(passwords => this.setState({ passwords }));
+        .then(res => res.json())
+        .then(data => this.setState({ data }));
+  }
+
+  unsupported() {
+    window.alert("This fantasy provider is not supported at this time.");
+  }
+
+  render() {
+    return (
+      <div>
+        <Typography variant="headline">Login with a Fantasy Football Provider to Add a Team</Typography>
+        <p><Button onClick={() => this.getYahooData()}>Add Yahoo Team</Button></p>
+        <p><Button onClick={() => this.unsupported()} style={{color: '#bbbbbb'}}>Add NFL Team</Button></p>
+        <p><Button onClick={() => this.unsupported()} style={{color: '#bbbbbb'}}>Add CBS Team</Button></p>
+        <p><Button onClick={() => this.unsupported()} style={{color: '#bbbbbb'}}>Add ESPN Team</Button></p>     
+      </div>
+    );
+  }
+}
+  
