717f9ec5db7c55d16ef92d6d7f5a512363340f09
camdent719 <34377169+camdent719@users.noreply.github.com>
2018-05-02 10:25:08 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ var fantasyData = {}; // global variable where our fantasy data will be stored
 
 var yf = new YantasySports();
 
+console.log("*** server.js: yf obj made");
+
 var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
